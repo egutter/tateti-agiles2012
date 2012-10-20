594910b86ea4bd0f68623b09ad598be262ad75b3
Emilio Gutter <egutter@10pines.com>
2012-10-20 17:20:33 -0300

    lugar ocupado

--- tateti_spec.rb
+++ tateti_spec.rb
@@ -1,24 +1,43 @@
 class Board
+
 	def initialize
 		@empty = true
 	end
-	
+
 	def empty?
 		@empty
 	end
+
 	def cross_plays_at(x, y)
 		@empty = false
 	end
 
+	def circle_plays_at(x, y)
+		raise PosicionOcupadaError.new('el lugar esta ocupado')
+	end
 end
 
+class PosicionOcupadaError < StandardError
+end
 describe "Tateti" do
-	it "un nuevo tablero debe estar vacio" do
-		Board.new.empty?.should == true
+
+	subject :board do
+		Board.new
 	end
+
+	it "un nuevo tablero debe estar vacio" do
+		board.empty?.should == true
+	end
+
 	it "un tablero con una jugada no deberia estar vacio" do
-		board = Board.new
 		board.cross_plays_at(0,0)
 		board.empty?.should == false
+	end
+
+	it "no se puede jugar en el mismo lugar" do
+		board.cross_plays_at(0,0)
+		expect {
+			board.circle_plays_at(0,0)
+		}.to raise_error(PosicionOcupadaError)
 	end
 end
