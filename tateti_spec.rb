5de1514e7c4ac8f363b3bf0a3190fbd7fa288485
Emilio Gutter <egutter@10pines.com>
2012-10-20 17:15:07 -0300

    table no vacio

--- tateti_spec.rb
+++ tateti_spec.rb
@@ -1,10 +1,24 @@
 class Board
-	def empty?
-		true
+	def initialize
+		@empty = true
 	end
+	
+	def empty?
+		@empty
+	end
+	def cross_plays_at(x, y)
+		@empty = false
+	end
+
 end
+
 describe "Tateti" do
 	it "un nuevo tablero debe estar vacio" do
-		Board.new.empty? == true
+		Board.new.empty?.should == true
+	end
+	it "un tablero con una jugada no deberia estar vacio" do
+		board = Board.new
+		board.cross_plays_at(0,0)
+		board.empty?.should == false
 	end
 end
