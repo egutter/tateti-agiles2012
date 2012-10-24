5e7a36910d750be751ef06d35103c726e3b63f5a
Martin Salias <martin@salias.com.ar>
2012-10-24 11:29:21 -0300

    Test básico

--- tateti_test.rb
+++ tateti_test.rb
@@ -1,4 +1,12 @@
+require "test/unit"
 
+require_relative "tateti"
 
+class TestTateti < Test::Unit::TestCase
 
+  def test_rue
+  # assert_equal "V", 5.to_roman
 
+    assert_equal true, true
+  end
+end
