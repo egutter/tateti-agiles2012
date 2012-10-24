1eb9f05a373e0212259386a87d86a68fad551e15
Martin Salias <martin@salias.com.ar>
2012-10-24 11:49:05 -0300

    Primer ficha

--- tateti_test.rb
+++ tateti_test.rb
@@ -7,11 +7,25 @@ class TestTateti < Test::Unit::TestCase
 
     assert_equal true, tablero.vacio?
   end
+
+  def test_pongo_algo_no_esta_vacio
+ 	tablero = Tablero.new
+	tablero.insertar()
+	assert_equal false, tablero.vacio?  	
+  end
 end
 
 class Tablero
+	
+	def initialize
+		@@vacio = true
+	end
 
 	def vacio?
-		true
+		@@vacio
+	end
+
+	def insertar
+		@@vacio = false
 	end
 end
