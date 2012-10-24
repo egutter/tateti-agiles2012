3280a79e9e29f5f396664c2994778ff62e564a1e
Emilio Gutter <egutter@10pines.com>
2012-10-24 12:05:07 -0300

    blabla

--- spec/tateti_spec.rb
+++ spec/tateti_spec.rb
@@ -1,7 +1,10 @@
 describe "Tateti" do
 
-	it "test should pass" do
-		true.should == true
-		
+	it "should be an array 3x3" do
+		board = [[],[],[]] 
+		board.size.should == 3
+		board[0].should be_a(Array)
+		board[1].should be_a(Array)
+		board[2].should be_a(Array)
 	end
 end
