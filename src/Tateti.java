f67b57023c9b861db9d32d9fee81e5248d88ac8a
Carlos Lopes <carlosaml@gmail.com>
2012-10-24 12:35:56 -0200

    backing array

--- src/Tateti.java
+++ src/Tateti.java
@@ -1,11 +1,17 @@
 
 public class Tateti {
 
-    public void set(int x, int y, char type) {
+    private char[][] juego;
 
+    public Tateti() {
+        juego = new char[3][3];
+    }
+
+    public void set(int x, int y, char type) {
+        juego[x][y] = type;
     }
 
     public char getAt(int x, int y) {
-        return 'X';
+        return juego[x][y];
     }
 }
--- src/TatetiTest.java
+++ src/TatetiTest.java
@@ -6,11 +6,20 @@ import static org.junit.Assert.assertThat;
 public class TatetiTest {
 
     @Test
-    public void shouldSetAndKeepPosition() {
+    public void shouldSetAndKeepPositionIfItIsX() {
         Tateti tateti = new Tateti();
 
         tateti.set(2, 2, 'X');
 
         assertThat(tateti.getAt(2, 2), is('X'));
+    }
+
+    @Test
+    public void shouldSetAndKeepPositionIfItIsY() {
+        Tateti tateti = new Tateti();
+
+        tateti.set(1, 1, 'Y');
+
+        assertThat(tateti.getAt(1, 1), is('Y'));
     }
 }
