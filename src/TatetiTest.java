9a72400e5cf1026af21ae92d5c9fd685b84063df
Carlos Lopes <carlosaml@gmail.com>
2012-10-24 12:28:12 -0200

    first commit

---
+++ src/TatetiTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TatetiTest {
+
+    @Test
+    public void shouldPass() {
+        assertThat(1, is(1));
+    }
+}
