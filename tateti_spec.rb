c70eba829f0ef5d357f3da03f1773cccbc89731c
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-10-24 11:38:12 -0300

    refactor tests

--- tateti_spec.rb
+++ tateti_spec.rb
@@ -1,27 +1,28 @@
 require './tateti'
 
 describe 'tateti' do
+
+	before :each do
+		@board = Tateti.new
+	end
+
 	it "should be able to start a new game" do
-		board = Tateti.new
-		board.respond_to?('new_game').should be_true
+		@board.respond_to?('new_game').should be_true
 	end
 
 	it "should be 3x3 dimension" do
-		board = Tateti.new
-		board.cols.should == 3
-		board.rows.should == 3
+		@board.cols.should == 3
+		@board.rows.should == 3
 	end
 
 	it "should be empty when starting a new game" do
-		board = Tateti.new
-		board.new_game
-		board.empty?.should be_true
+		@board.new_game
+		@board.empty?.should be_true
 	end
 
 	it "should allow play X in 1,1 cell" do
-		board = Tateti.new
-		board.play 'X', 1, 1
-		board.value_at(1,1).should == 'X'
+		@board.play 'X', 1, 1
+		@board.value_at(1,1).should == 'X'
 	end
 
 
