fa6bac89a68dfe1294dd166ed22f339f522b98a8
Carlos Lopes <carlosaml@gmail.com>
2012-10-24 12:32:50 -0200

    first test

--- src/Tateti.java
+++ src/Tateti.java
@@ -1,3 +1,11 @@
 
 public class Tateti {
+
+    public void set(int x, int y, char type) {
+
+    }
+
+    public char getAt(int x, int y) {
+        return 'X';
+    }
 }
--- src/TatetiTest.java
+++ src/TatetiTest.java
@@ -6,7 +6,11 @@ import static org.junit.Assert.assertThat;
 public class TatetiTest {
 
     @Test
-    public void shouldPass() {
-        assertThat(1, is(1));
+    public void shouldSetAndKeepPosition() {
+        Tateti tateti = new Tateti();
+
+        tateti.set(2, 2, 'X');
+
+        assertThat(tateti.getAt(2, 2), is('X'));
     }
 }
